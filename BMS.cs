2555b1aacbbe3a58768661cf6d72981788281f29
dat09loz <61342271+dat09loz@users.noreply.github.com>
2022-09-09 16:06:34 +1000

    login menu

---
+++ BMS.cs
@@ -0,0 +1,44 @@
+﻿using System;
+// using System.Collections;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Assg1_ConsoleApplication
+{
+    public class BMS
+    {
+        private List<BankAccount> accounts;
+
+        public BMS()
+        {
+            accounts = new List<BankAccount>();
+        }
+        static void Main(string[] args)
+        {
+            new BMS().LoginMenu();
+        }
+
+        private void LoginMenu()
+        {
+            OutputLoginMenu();
+        }
+
+        private void OutputLoginMenu()
+        {
+            Console.WriteLine("___________________________________________________");
+            Console.WriteLine("|                                                  |");
+            Console.WriteLine("|     WELCOME TO SIMPLE BANK MANAGEMENT SYSTEM     |");
+            Console.WriteLine("|                                                  |");
+            Console.WriteLine("___________________________________________________");
+            Console.WriteLine("|                                                  |");
+            Console.WriteLine("|                      SIGN IN                     |");
+            Console.WriteLine("|     Username:                                    |");
+            Console.WriteLine("|     Password:                                    |");
+            Console.WriteLine("|                                                  |");
+            Console.WriteLine("___________________________________________________");
+
+
+        }
+    }
+}
---
+++ MenuItemsEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assg1_ConsoleApplication
+{
+    // this enum will handle menu option input (if the user press option 4, withdraw method will be called etc.)
+    public enum MenuOptions 
+    {
+        SimpleBankingSystem,
+        CreateAccount,
+        SearchAccount,
+        Deposit,
+        Withdraw,
+        ACStatement,
+        DeleteAccount,
+        Exit
+    }
+}
